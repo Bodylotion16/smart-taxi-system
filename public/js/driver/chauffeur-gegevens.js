8b2d9ca02534a25c05c76d05de9a0b36418b4513
Anisha Patandin <anisha.patandin13@gmail.com>
2026-05-21 22:25:12 -0300

    chauffeur-gegevens

---
+++ public/js/driver/chauffeur-gegevens.js
@@ -0,0 +1,7 @@
+function editProfile() {
+    alert("Profiel aanpassen wordt later gekoppeld aan de backend.");
+}
+
+function changePassword() {
+    alert("Wachtwoord wijzigen wordt later toegevoegd.");
+}
