8b72f1ae009cf0d776fd47b96604e3bf2a1beb88
Anisha Patandin <anisha.patandin13@gmail.com>
2026-05-23 19:37:21 -0300

    driver actieve-rit changes

--- public/js/driver/actieve-rit.js
+++ public/js/driver/actieve-rit.js
@@ -1,7 +1,19 @@
-function changeRideStatus(newStatus) {
+function changeRideStatus(newStatus, button) {
     const statusElement = document.getElementById("rideStatus");
 
     statusElement.textContent = newStatus;
 
-    alert("Ritstatus gewijzigd naar: " + newStatus);
+    const buttons = document.querySelectorAll(".driver-status-flow button");
+
+    buttons.forEach(function(btn) {
+        btn.classList.remove("selected-status");
+    });
+
+    button.classList.add("selected-status");
+
+    if (newStatus === "Rit afgerond") {
+        statusElement.classList.add("completed");
+    } else {
+        statusElement.classList.remove("completed");
+    }
 }
